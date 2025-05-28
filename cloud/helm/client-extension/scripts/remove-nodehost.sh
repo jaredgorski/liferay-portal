f9c19cb747afaae712217288a68928de9cdc04e9
jaredgorski <jared.gorski@liferay.com>
2025-05-27 22:26:36 -0400

    LCD-46569 Add post-install and pre-delete hooks to manipulate coredns when installing/uninstalling CX

---
+++ cloud/helm/client-extension/scripts/remove-nodehost.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+NODEHOST_TO_REMOVE="$1"
+
+coredns_configmap_file=$(mktemp /tmp/coredns-configmap.XXXXXX)
+new_coredns_configmap_file="${coredns_configmap_file}.new"
+
+kubectl get configmap coredns -n kube-system -o yaml >"$coredns_configmap_file"
+
+NODEHOST_TO_REMOVE_LINE="    172.18.0.3 ${NODEHOST_TO_REMOVE}"
+
+cat "$coredns_configmap_file" | sed -e "/${NODEHOST_TO_REMOVE_LINE}/d" >"$new_coredns_configmap_file"
+
+kubectl apply -f "$new_coredns_configmap_file"
+
+kubectl rollout restart deployments/coredns -n kube-system
