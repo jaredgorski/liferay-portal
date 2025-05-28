c0c0cc824c1dc73c11f26e0291983ad28cc87ac7
jaredgorski <jared.gorski@liferay.com>
2025-05-27 22:30:17 -0400

    LCD-46569 Minor update

--- cloud/helm/client-extension/scripts/append-nodehost.sh
+++ cloud/helm/client-extension/scripts/append-nodehost.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 NODEHOST_TO_APPEND="$1"
+K3D_INTERNAL_HOST_IP="172.18.0.3"
 
 coredns_configmap_file=$(mktemp /tmp/coredns-configmap.XXXXXX)
 new_coredns_configmap_file="${coredns_configmap_file}.new"
@@ -16,7 +17,7 @@ desanitize () {
 }
 
 NODEHOSTS_KEY="NodeHosts: |"
-NEW_NODEHOST_LINE="    172.18.0.3 ${NODEHOST_TO_APPEND}"
+NEW_NODEHOST_LINE="    ${K3D_INTERNAL_HOST_IP} ${NODEHOST_TO_APPEND}"
 NEW_NODEHOSTS_SECTION=$(printf '%s\n%s' "$NODEHOSTS_KEY" "$NEW_NODEHOST_LINE")
 NEW_NODEHOSTS_SECTION_SANITIZED=$(echo -n "$NEW_NODEHOSTS_SECTION" | sanitize)
 
