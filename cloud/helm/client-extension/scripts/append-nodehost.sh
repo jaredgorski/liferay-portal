17b17d9cb2c5cd755065e74c799b9dfc94f8515e
jaredgorski <jared.gorski@liferay.com>
2025-05-28 09:08:56 -0400

    LCD-46569 Remove need to match on explicit IP

--- cloud/helm/client-extension/scripts/append-nodehost.sh
+++ cloud/helm/client-extension/scripts/append-nodehost.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 NODEHOST_TO_APPEND="$1"
-K3D_INTERNAL_HOST_IP="172.18.0.3"
+
+K3D_INTERNAL_HOST="host.k3d.internal"
 
 coredns_configmap_file=$(mktemp /tmp/coredns-configmap.XXXXXX)
 new_coredns_configmap_file="${coredns_configmap_file}.new"
@@ -16,12 +17,12 @@ desanitize () {
 	tr '\f' '\n'
 }
 
-NODEHOSTS_KEY="NodeHosts: |"
-NEW_NODEHOST_LINE="    ${K3D_INTERNAL_HOST_IP} ${NODEHOST_TO_APPEND}"
-NEW_NODEHOSTS_SECTION=$(printf '%s\n%s' "$NODEHOSTS_KEY" "$NEW_NODEHOST_LINE")
+K3D_INTERNAL_HOST_LINE=$(sed -n "/${K3D_INTERNAL_HOST}\$/p" "$coredns_configmap_file")
+NEW_NODEHOST_LINE="${K3D_INTERNAL_HOST_LINE/$K3D_INTERNAL_HOST/$NODEHOST_TO_APPEND}"
+NEW_NODEHOSTS_SECTION=$(printf '%s\n%s' "$K3D_INTERNAL_HOST_LINE" "$NEW_NODEHOST_LINE")
 NEW_NODEHOSTS_SECTION_SANITIZED=$(printf "$NEW_NODEHOSTS_SECTION" | sanitize)
 
-cat "$coredns_configmap_file" | sed -e "s/${NODEHOSTS_KEY}/${NEW_NODEHOSTS_SECTION_SANITIZED}/g" | desanitize >"$new_coredns_configmap_file"
+cat "$coredns_configmap_file" | sed -e "s/${K3D_INTERNAL_HOST_LINE}/${NEW_NODEHOSTS_SECTION_SANITIZED}/" | desanitize >"$new_coredns_configmap_file"
 
 kubectl apply -f "$new_coredns_configmap_file"
 
