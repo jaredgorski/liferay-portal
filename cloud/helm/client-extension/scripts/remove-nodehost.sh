9696e80033cc04ec482a75c252b3de1b29f026b8
jaredgorski <jared.gorski@liferay.com>
2025-05-28 09:08:56 -0400

    LCD-46569 Remove need to match on explicit IP

--- cloud/helm/client-extension/scripts/remove-nodehost.sh
+++ cloud/helm/client-extension/scripts/remove-nodehost.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 NODEHOST_TO_REMOVE="$1"
-K3D_INTERNAL_HOST_IP="172.18.0.3"
 
 coredns_configmap_file=$(mktemp /tmp/coredns-configmap.XXXXXX)
 new_coredns_configmap_file="${coredns_configmap_file}.new"
 
 kubectl get configmap coredns -n kube-system -o yaml >"$coredns_configmap_file"
 
-NODEHOST_TO_REMOVE_LINE="    ${K3D_INTERNAL_HOST_IP} ${NODEHOST_TO_REMOVE}"
-
-cat "$coredns_configmap_file" | sed -e "/${NODEHOST_TO_REMOVE_LINE}/d" >"$new_coredns_configmap_file"
+cat "$coredns_configmap_file" | sed -e "/${NODEHOST_TO_REMOVE}\$/d" >"$new_coredns_configmap_file"
 
 kubectl apply -f "$new_coredns_configmap_file"
 
