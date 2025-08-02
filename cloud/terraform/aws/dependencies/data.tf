f1a30f62f6820e67a09ca128eec2d881c64aecef
jaredgorski <jared.gorski@liferay.com>
2025-08-01 20:10:21 -0400

    LPD-61840 Implement AWS UX terraform improvements

---
+++ cloud/terraform/aws/dependencies/data.tf
@@ -0,0 +1,10 @@
+data "aws_caller_identity" "current" {
+}
+data "aws_eks_cluster" "cluster" {
+	name=var.cluster_name
+}
+data "aws_eks_cluster_auth" "cluster" {
+	name=var.cluster_name
+}
+data "aws_region" "current" {
+}
