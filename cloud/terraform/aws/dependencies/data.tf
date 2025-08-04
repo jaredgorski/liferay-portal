1e8d054e4fde4f92b627f6111ba2d766c387faa4
jaredgorski <jared.gorski@liferay.com>
2025-08-04 11:14:48 -0400

    LPD-61840 Disambiguate cluster_auth local name

--- cloud/terraform/aws/dependencies/data.tf
+++ cloud/terraform/aws/dependencies/data.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {
 data "aws_eks_cluster" "cluster" {
 	name=var.cluster_name
 }
-data "aws_eks_cluster_auth" "cluster" {
+data "aws_eks_cluster_auth" "cluster_auth" {
 	name=var.cluster_name
 }
 data "aws_region" "current" {
