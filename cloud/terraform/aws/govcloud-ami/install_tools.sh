8418a028fb5d0af06a080b5008d6d800031362af
Raymond Augé <raymond.auge@liferay.com>
2025-09-11 10:07:21 -0400

    LPD-64926 install tree and kubernetes client

--- cloud/terraform/aws/govcloud-ami/install_tools.sh
+++ cloud/terraform/aws/govcloud-ami/install_tools.sh
@@ -2,7 +2,7 @@
 
 # Install dependencies
 sudo yum update --assumeyes
-sudo yum install --assumeyes git shadow-utils unzip yum-utils
+sudo yum install --assumeyes git tree shadow-utils unzip yum-utils
 
 # Install AWS CLI
 curl --fail-with-body --location --show-error --silent "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" --output "awscliv2.zip"
@@ -16,6 +16,12 @@ curl --fail-with-body --location --show-error --silent "https://releases.hashico
 unzip terraform.zip
 sudo mv terraform /usr/local/bin/
 rm terraform.zip
+
+# Install Kubernetes Client
+KUBERNETES_VERSION="1.23.6"
+curl --fail-with-body --location --show-error --silent "https://dl.k8s.io/release/v${KUBERNETES_VERSION}/bin/linux/amd64/kubectl" --output "kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
 
 # Install eksctl
 curl --fail-with-body --location --show-error --silent "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz" --output "eksctl.tar.gz"
