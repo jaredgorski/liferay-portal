b651ca56f5f72cf2a386dd2adea7253636f98a1c
Raymond Augé <raymond.auge@liferay.com>
2025-09-11 16:29:59 -0400

    LPD-64926 run the eks portion of the terraform

--- cloud/terraform/aws/govcloud-ami/run-on-boot.sh
+++ cloud/terraform/aws/govcloud-ami/run-on-boot.sh
@@ -10,3 +10,25 @@ REGION=$(     curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/
 echo "AMI ID: $AMI_ID"
 echo "Instance ID: $INSTANCE_ID"
 echo "Region: $REGION"
+echo "AMI Profile: $(aws sts get-caller-identity)"
+
+tree /opt/liferay
+
+(
+	cd /opt/liferay/terraform/eks
+
+	if ! terraform show -json | jq -e '.values'
+	then
+		terraform init
+		terraform apply -auto-approve \
+			-var="deployment_name=lfr-ami-${INSTANCE_ID}" \
+			-var="region=${REGION}"
+		terraform output > ../dependencies/terraform.tfvars
+	fi
+
+	aws eks update-kubeconfig \
+		--name $(terraform output -raw cluster_name) \
+		--region $(terraform output -raw region)
+
+	kubectl cluster-info
+)
