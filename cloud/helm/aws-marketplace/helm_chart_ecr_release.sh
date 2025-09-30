ad431605dff97e6e0702ffd614153c014ab3c6ee
jaredgorski <jared.gorski@liferay.com>
2025-09-30 13:53:45 -0400

    LPD-66870 Update

--- cloud/helm/aws-marketplace/helm_chart_ecr_release.sh
+++ cloud/helm/aws-marketplace/helm_chart_ecr_release.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 export AWS_PROFILE=AWSAdministratorAccess-831926597587
-export HELM_EXPERIMENTAL_OCI=1
 
 CHART_NAME=$(cat ./Chart.yaml | yq .name)
 CHART_VERSION=$(cat ./Chart.yaml | yq .version)
 ECR_HOST=709825985650.dkr.ecr.us-east-1.amazonaws.com
-ECR_REPOSITORY="liferay/charts"
+ECR_REPOSITORY="liferay/charts5"
 PACKAGED_CHART_FILENAME="${CHART_NAME}-${CHART_VERSION}.tgz"
-RELEASE_DESTINATION="${ECR_HOST}/${ECR_REPOSITORY}:${CHART_VERSION}"
+RELEASE_DESTINATION="${ECR_HOST}/${ECR_REPOSITORY}"
 
 cleanup() {
 	rm -rf ${PACKAGED_CHART_FILENAME} Chart.lock charts
@@ -24,7 +23,7 @@ configure_aws_ecr() {
 push_chart_to_ecr() {
 	helm dependency update
 
-	helm package . --version "${CHART_VERSION}"
+	helm package .
 
 	helm push "${PACKAGED_CHART_FILENAME}" "oci://${RELEASE_DESTINATION}"
 
