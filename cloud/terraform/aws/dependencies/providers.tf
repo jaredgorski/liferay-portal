62851d69a5352048acbe6926bc109a72be1544c9
Raymond Augé <raymond.auge@liferay.com>
2025-10-22 14:52:34 -0400

    gitops prototype

--- cloud/terraform/aws/dependencies/providers.tf
+++ cloud/terraform/aws/dependencies/providers.tf
@@ -7,6 +7,9 @@ provider "aws" {
 	region=var.region
 }
 provider "kubernetes" {
+    cluster_ca_certificate=base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    host=data.aws_eks_cluster.cluster.endpoint
+    token=data.aws_eks_cluster_auth.cluster.token
 }
 terraform {
 	required_providers {
--- cloud/terraform/aws/eks/providers.tf
+++ cloud/terraform/aws/eks/providers.tf
@@ -6,11 +6,31 @@ provider "aws" {
 	}
 	region=var.region
 }
+provider "helm" {
+	kubernetes={
+		cluster_ca_certificate=module.eks.cluster_certificate_authority_data
+		host=module.eks.cluster_endpoint
+		token=data.aws_eks_cluster_auth.current.token
+	}
+}
+provider "kubernetes" {
+	cluster_ca_certificate=module.eks.cluster_certificate_authority_data
+	host=module.eks.cluster_endpoint
+	token=data.aws_eks_cluster_auth.current.token
+}
 terraform {
 	required_providers {
 		aws={
 			source="hashicorp/aws"
 			version="~> 6.14.1"
+		}
+		helm={
+			source="hashicorp/helm"
+			version="3.0.2"
+		}
+		kubernetes={
+			source="hashicorp/kubernetes"
+			version="~> 2.38.0"
 		}
 		random={
 			source="hashicorp/random"
